0162cb912717db83945b91a417260d94b4573306
LAPTOP-GHR86GEE\17387 <1738789165@qq.com>
2020-07-01 17:39:46 +0800

    feat: 引入mysql相关插件以及配置

--- config/config.default.js
+++ config/config.default.js
@@ -22,7 +22,25 @@ module.exports = appInfo => {
   const userConfig = {
     // myAppName: 'egg',
   };
-
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: '127.0.0.1',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: '831015',
+      // database
+      database: 'foreground',
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
   return {
     ...config,
     ...userConfig,
